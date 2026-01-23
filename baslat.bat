a356c5780cf3dd3c30c800b1989afe2c7c56c1d6
omerbabaco <omer.esenbuga41@gmail.com>
2026-01-23 23:08:00 +0300

    qqq

--- baslat.bat
+++ baslat.bat
@@ -1,6 +1,6 @@
 @echo off
 chcp 65001 >nul 2>&1
-title SS-EXT V2.1 - Baslat
+title SS-EXT V2.0 - Baslat
 
 :: Windows 10+ ANSI renk destegini aktif et
 reg add HKCU\Console /v VirtualTerminalLevel /t REG_DWORD /d 1 /f >nul 2>&1
@@ -8,7 +8,7 @@ reg add HKCU\Console /v VirtualTerminalLevel /t REG_DWORD /d 1 /f >nul 2>&1
 cls
 echo.
 echo  [96m========================================[0m
-echo  [93m       SS-EXT V2.1 - Baslatiliyor[0m
+echo  [93m       SS-EXT V2.0 - Baslatiliyor[0m
 echo  [90m       Yapimci: OMERBABACO[0m
 echo  [96m========================================[0m
 echo.
